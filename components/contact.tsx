0e451f65599f08f4afbc74cfb9600ae66bcf048e
Lucie Berkovitch <95320849+lucieberko@users.noreply.github.com>
2026-01-20 15:59:56 +0100

    Update contact page with psychedelic address

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,8 +1,4 @@
-// cd /Users/lb2239/Downloads/lucieberko.github.io-main
-// npm run dev​
-
 'use client';
-
 import { useTranslations } from 'next-intl';
 
 export default function Contact() {
@@ -12,27 +8,35 @@ export default function Contact() {
     <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-24 sm:py-32">
       <div className="mx-auto max-w-xl text-center">
         <h2 className="text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">{t('title')}</h2>
-
         <p className="mt-12 text-lg/8 text-gray-600">
           {t('intro')}
         </p>
-
         <p className="my-6 text-lg/8 font-medium font-mono bg-clip-text text-transparent bg-gradient-to-r from-sky-500 via-pink-500 to-yellow-500">
           <span>l.berkovitch</span>
           <span className="mx-2">@</span>
           <span>ghu-paris.fr</span>
         </p>
-
         <p className="my-6 text-lg/8 text-gray-600">
           {t('or')}
         </p>
-
         <p className="my-6 text-lg/8 font-medium font-mono bg-clip-text text-transparent bg-gradient-to-r from-amber-500 via-emerald-500 to-purple-500 text-left w-fit mx-auto">
-          Dr Lucie Berkovitch,<br />
+          {t('addressTitle')}<br />
           GHU Paris Psychiatrie & Neurosciences,<br />
           1, rue Cabanis,<br />
           75014 Paris, France
         </p>
+
+        {/* Nouvelle section pour les études psychédéliques */}
+        <div className="mt-24">
+          <p className="text-2xl font-semibold tracking-tight text-balance text-gray-900 sm:text-2xl">
+            {t('psychedelicStudies')}
+          </p>
+          <p className="mt-12 text-lg/8 font-medium font-mono bg-clip-text text-transparent bg-gradient-to-r from-sky-500 via-pink-500 to-yellow-500">
+            <span>recherchepsyche</span>
+            <span className="mx-2">@</span>
+            <span>ghu-paris.fr</span>
+          </p>
+        </div>
       </div>
     </div>
   );
